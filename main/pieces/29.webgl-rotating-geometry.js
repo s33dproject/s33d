28103671f86cfc33a831420b7262bb4e0e763b02
julietaflux <julietaflux@gmail.com>
2020-02-16 19:54:09 -0300

    names

--- main/pieces/29.webgl-rotating-geometry.js
+++ main/pieces/29.webgl-rotating-geometry.js
@@ -11,7 +11,7 @@ global.THREE = require('three');
 require('three/examples/js/controls/OrbitControls');
 
 const settings = {
-  dimensions: [ 512, 512],
+  dimensions: [2048, 2048],
   fps: 24,
   duration: 4,
   // Make the loop animated
@@ -41,47 +41,47 @@ const sketch = ({ context }) => {
 
   const box = new THREE.BoxGeometry(1, 1, 1);
 
-    for (let i = 0; i < 40; i++) {
-      const mesh = new THREE.Mesh(
-        box,
-        new THREE.MeshStandardMaterial({ // change to Basic/Standard Material for shadows
-          color: random.pick(palette),
-        })
-      );
-      mesh.position.set(
-        random.range(-1, 1), // x
-        random.range(-1, 1), // z
-        random.range(-1, 1) // y
-      );
-      mesh.scale.set(
-        random.range(-1, 1),
-        random.range(-1, 1),
-        random.range(-1, 1) 
-      );
-      mesh.scale.multiplyScalar(0.5)
-      scene.add(mesh);
-    }
+  for (let i = 0; i < 40; i++) {
+    const mesh = new THREE.Mesh(
+      box,
+      new THREE.MeshStandardMaterial({ // change to Basic/Standard Material for shadows
+        color: random.pick(palette),
+      })
+    );
+    mesh.position.set(
+      random.range(-1, 1), // x
+      random.range(-1, 1), // z
+      random.range(-1, 1) // y
+    );
+    mesh.scale.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+    mesh.scale.multiplyScalar(0.5)
+    scene.add(mesh);
+  }
 
-    scene.add(new THREE.AmbientLight('hsl(0, 0%, 20%'))
-    const light = new THREE.DirectionalLight('white', 1);
-    light.position.set(2, 2, 4)
-    scene.add(light);
-
-    const easeFn = BezierEasing(0.67, 0.03, 0.29, 0.99);
-
-/*
-  // Specify an ambient/unlit colour
-  scene.add(new THREE.AmbientLight('#59314f'));
-
-  // Add some light
-  const light = new THREE.PointLight('#45caf7', 1, 15.5);
-  light.position.set(2, 2, -4).multiplyScalar(1.5);
+  scene.add(new THREE.AmbientLight('hsl(0, 0%, 20%'))
+  const light = new THREE.DirectionalLight('white', 1);
+  light.position.set(2, 2, 4)
   scene.add(light);
-*/
+
+  const easeFn = BezierEasing(0.67, 0.03, 0.29, 0.99);
+
+  /*
+    // Specify an ambient/unlit colour
+    scene.add(new THREE.AmbientLight('#59314f'));
+  
+    // Add some light
+    const light = new THREE.PointLight('#45caf7', 1, 15.5);
+    light.position.set(2, 2, -4).multiplyScalar(1.5);
+    scene.add(light);
+  */
   // draw each frame
   return {
     // Handle resize events here
-    resize ({ pixelRatio, viewportWidth, viewportHeight }) {
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
 
@@ -108,15 +108,15 @@ const sketch = ({ context }) => {
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
-    render ({ playhead }) {
-     // mesh.rotation.y = time * (10 * Math.PI / 180);
+    render({ playhead }) {
+      // mesh.rotation.y = time * (10 * Math.PI / 180);
       const t = Math.sin(playhead * Math.PI * 2);
       scene.rotation.z = easeFn(t);
       //scene.rotation.y = playhead * (Math.PI * 2),
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
-    unload () {
+    unload() {
       renderer.dispose();
     }
   };
